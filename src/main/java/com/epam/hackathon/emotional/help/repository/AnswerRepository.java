ebc0146da25d5aedbc5eabc2dbf19c52cdd927b9
Volodymyr Bolshakov <volodbol@gmail.com>
2022-11-24 14:52:41 +0200

    fix: delete unused repository

--- src/main/java/com/epam/hackathon/emotional/help/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epam.hackathon.emotional.help.repository;
-
-import com.epam.hackathon.emotional.help.model.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
--- src/main/java/com/epam/hackathon/emotional/help/repository/AnswersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epam.hackathon.emotional.help.repository;
-
-import com.epam.hackathon.emotional.help.model.Answers;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswersRepository extends JpaRepository<Answers, Long> {
-}
