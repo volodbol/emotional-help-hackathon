8e3a877baf8a83053ef88ca4d07af445023e26f6
Volodymyr Bolshakov <volodbol@gmail.com>
2022-11-24 14:52:15 +0200

    feat: add repositories for models

--- src/main/java/com/epam/hackathon/emotional/help/repository/EmotionalMapRepository.java
+++ src/main/java/com/epam/hackathon/emotional/help/repository/EmotionalMapRepository.java
@@ -1,9 +1,9 @@
 package com.epam.hackathon.emotional.help.repository;
 
-import com.epam.hackathon.emotional.help.model.Questionnaire;
+import com.epam.hackathon.emotional.help.model.EmotionalMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long> {
+public interface EmotionalMapRepository extends JpaRepository<EmotionalMap, Long> {
 }
