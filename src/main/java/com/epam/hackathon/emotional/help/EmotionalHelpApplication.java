52345febf13f888d78c27067b43085539db3449f
Volodymyr Bolshakov <volodbol@gmail.com>
2022-11-24 12:19:16 +0200

    feat: add basic spring project

---
+++ src/main/java/com/epam/hackathon/emotional/help/EmotionalHelpApplication.java
@@ -0,0 +1,13 @@
+package com.epam.hackathon.emotional.help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionalHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotionalHelpApplication.class, args);
+    }
+
+}
