5fb7e3612f1b8b8306f674d39ffbf9102eecfdb1
Volodymyr Bolshakov <volodbol@gmail.com>
2022-11-24 12:52:38 +0200

    fix: add constructor to user service and delete methods

--- src/main/java/com/epam/hackathon/emotional/help/service/ApplicationUserService.java
+++ src/main/java/com/epam/hackathon/emotional/help/service/ApplicationUserService.java
@@ -1,20 +1,14 @@
 package com.epam.hackathon.emotional.help.service;
 
-import com.epam.hackathon.emotional.help.model.ApplicationUser;
 import com.epam.hackathon.emotional.help.repository.ApplicationUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ApplicationUserService {
-    @Autowired
-    ApplicationUserRepository applicationUserRepository;
 
-    public ApplicationUser findUserByUsername(String username) {
-        return applicationUserRepository.findByUsername(username);
-    }
+    private final ApplicationUserRepository applicationUserRepository;
 
-    public ApplicationUser addUser(ApplicationUser user) {
-        return applicationUserRepository.save(user);
-    }
+
 }
