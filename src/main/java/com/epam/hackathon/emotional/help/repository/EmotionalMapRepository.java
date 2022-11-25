42024189dba8331b7a147de1c2093302aff34fde
Volodymyr Bolshakov <volodbol@gmail.com>
2022-11-25 10:42:55 +0200

    feat: add missing repositories

--- src/main/java/com/epam/hackathon/emotional/help/repository/EmotionalMapRepository.java
+++ src/main/java/com/epam/hackathon/emotional/help/repository/EmotionalMapRepository.java
@@ -1,9 +1,17 @@
 package com.epam.hackathon.emotional.help.repository;
 
+import com.epam.hackathon.emotional.help.model.ApplicationUser;
 import com.epam.hackathon.emotional.help.model.EmotionalMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmotionalMapRepository extends JpaRepository<EmotionalMap, Long> {
+
+    Optional<EmotionalMap> findByAnonymousUUID(String uuid);
+
+    Optional<EmotionalMap> findByApplicationUser(ApplicationUser applicationUser);
+
 }
