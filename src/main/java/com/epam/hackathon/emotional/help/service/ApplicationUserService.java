8c6466b855bd23c50f82198dda27e0b94bcf546f
Volodymyr Bolshakov <volodbol@gmail.com>
2022-11-25 10:44:25 +0200

    feat: add and refactor services

--- src/main/java/com/epam/hackathon/emotional/help/service/ApplicationUserService.java
+++ src/main/java/com/epam/hackathon/emotional/help/service/ApplicationUserService.java
@@ -1,8 +1,11 @@
 package com.epam.hackathon.emotional.help.service;
 
+import com.epam.hackathon.emotional.help.model.ApplicationUser;
 import com.epam.hackathon.emotional.help.repository.ApplicationUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +13,9 @@ public class ApplicationUserService {
 
     private final ApplicationUserRepository applicationUserRepository;
 
+
+    public Optional<ApplicationUser> findUserById(Long id) {
+        return applicationUserRepository.findById(id);
+    }
 
 }
