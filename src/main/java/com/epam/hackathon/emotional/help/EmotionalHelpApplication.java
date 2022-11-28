0772b546cb56d107d16a8044567c3b25a98cd6c7
Azizbek <azizbek_khushvakov@epam.com>
2022-11-28 14:34:04 +0500

    resolved conflict

--- src/main/java/com/epam/hackathon/emotional/help/EmotionalHelpApplication.java
+++ src/main/java/com/epam/hackathon/emotional/help/EmotionalHelpApplication.java
@@ -1,7 +1,18 @@
 package com.epam.hackathon.emotional.help;
 
+import com.epam.hackathon.emotional.help.model.Answer;
+import com.epam.hackathon.emotional.help.model.AnswerGroup;
+import com.epam.hackathon.emotional.help.model.Question;
+import com.epam.hackathon.emotional.help.model.Questionnaire;
+import com.epam.hackathon.emotional.help.repository.AnswerRepository;
+import com.epam.hackathon.emotional.help.repository.QuestionRepository;
+import com.epam.hackathon.emotional.help.repository.QuestionnaireRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class EmotionalHelpApplication {
@@ -9,5 +20,61 @@ public class EmotionalHelpApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmotionalHelpApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner runner(
+            QuestionnaireRepository questionnaireRepository,
+            QuestionRepository questionRepository,
+            AnswerRepository answerRepository) {
+        List<Answer> answers = answers();
+        answerRepository.saveAll(answers);
+        List<Question> questions = questions(answerRepository);
+        questionRepository.saveAll(questions);
+        return (String[] args) -> questionnaireRepository.save(
+                Questionnaire.builder()
+                        .title("Emotional map")
+                        .questions(questions)
+                        .build()
+        );
+    }
+
+    private static List<Question> questions(AnswerRepository answerRepository) {
+        return List.of(
+                Question.builder()
+                        .questionText("Question 1")
+                        .answers(answerRepository.findAll().subList(0, 2))
+                        .build(),
+                Question.builder()
+                        .questionText("Question 2")
+                        .answers(answerRepository.findAll().subList(2, 4))
+                        .build()
+        );
+    }
+
+    private static List<Answer> answers() {
+        return List.of(
+                Answer.builder()
+                        .value("Ok")
+                        .mark(3)
+                        .answerGroup(AnswerGroup.HAPPY)
+                        .build(),
+                Answer.builder()
+                        .value("Bad")
+                        .mark(4)
+                        .answerGroup(AnswerGroup.SADNESS)
+                        .build(),
+                Answer.builder()
+                        .value("Fine")
+                        .mark(3)
+                        .answerGroup(AnswerGroup.JOY)
+                        .build(),
+                Answer.builder()
+                        .value("No hope")
+                        .mark(4)
+                        .answerGroup(AnswerGroup.SADNESS)
+                        .build()
+        );
+    }
+
 
 }
