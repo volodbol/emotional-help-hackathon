baa8c07722a96715d53ab00fc7e3465aef1804c2
Volodymyr Bolshakov <volodbol@gmail.com>
2022-11-29 13:33:14 +0200

    fix: add constructors for dtos

--- src/main/java/com/epam/hackathon/emotional/help/dto/AnswerDto.java
+++ src/main/java/com/epam/hackathon/emotional/help/dto/AnswerDto.java
@@ -1,11 +1,15 @@
 package com.epam.hackathon.emotional.help.dto;
 
 import com.epam.hackathon.emotional.help.model.AnswerGroup;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnswerDto {
 
     private Integer mark;
