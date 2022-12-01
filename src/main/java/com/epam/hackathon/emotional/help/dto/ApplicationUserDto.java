1b37ed395840e17f8eca69d960b1a5c054c9a338
Kudakster <vitaliy.chervatyuk@gmail.com>
2022-12-01 11:59:43 +0200

    fix: add constructors for dto

--- src/main/java/com/epam/hackathon/emotional/help/dto/ApplicationUserDto.java
+++ src/main/java/com/epam/hackathon/emotional/help/dto/ApplicationUserDto.java
@@ -1,10 +1,14 @@
 package com.epam.hackathon.emotional.help.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApplicationUserDto {
 
     private String username;
