06f929bf71ac9305abbc87b756376941f1ee2c97
Kudakster <vitaliy.chervatyuk@gmail.com>
2022-12-01 11:36:19 +0200

    feat: configure security with uuid token feat: add public and protected urls feat: add BCryptPasswordEncoder feat: add UUID filter feat: add CustomAuthManager

--- src/main/java/com/epam/hackathon/emotional/help/configuration/SecurityConfiguration.java
+++ src/main/java/com/epam/hackathon/emotional/help/configuration/SecurityConfiguration.java
@@ -1,19 +1,47 @@
 package com.epam.hackathon.emotional.help.configuration;
 
+import com.epam.hackathon.emotional.help.filter.UUIDAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 
 @EnableWebSecurity
-public class SecurityConfiguration {
-
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfiguration {
+    private static final String PUBLIC_URLS = "/users";
+    private static final RequestMatcher PROTECTED_URLS = new AntPathRequestMatcher("/questionnaires/**");
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.formLogin().disable()
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
+                .authorizeRequests((req) -> req
+                        .antMatchers(PUBLIC_URLS).permitAll())
+                .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
                 .csrf().disable()
-                .antMatcher("/**").authorizeRequests().anyRequest().permitAll();
+                .httpBasic().disable();
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UUIDAuthenticationFilter restAuthenticationFilter() {
+        return new UUIDAuthenticationFilter(PROTECTED_URLS);
+    }
 }
---
+++ src/main/java/com/epam/hackathon/emotional/help/filter/UUIDAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.epam.hackathon.emotional.help.filter;
+
+import com.epam.hackathon.emotional.help.service.ApplicationUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+public class UUIDAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    @Autowired
+    private ApplicationUserService applicationUserService;
+
+    public UUIDAuthenticationFilter(RequestMatcher defaultFilterProcessesUrl) {
+        super(defaultFilterProcessesUrl);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        Optional<String> param = Optional.ofNullable(request.getHeader(AUTHORIZATION));
+        String token = param
+                .map(String::trim)
+                .filter(applicationUserService::isUuidExist)
+                .orElseThrow(() -> new BadCredentialsException("Invalid Authentication Token"));
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(token, token);
+        return getAuthenticationManager().authenticate(auth);
+    }
+
+    @Override
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+        chain.doFilter(request, response);
+    }
+}
