c9fc369a44c7caaf6d4c78c6b9b5934902b6945d
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-01 12:26:00 +0200

    feat: make questionnaire controller return the dto

--- src/main/java/com/epam/hackathon/emotional/help/controller/QuestionnaireController.java
+++ src/main/java/com/epam/hackathon/emotional/help/controller/QuestionnaireController.java
@@ -1,9 +1,11 @@
 package com.epam.hackathon.emotional.help.controller;
 
+import com.epam.hackathon.emotional.help.dto.QuestionnaireDto;
 import com.epam.hackathon.emotional.help.exception.QuestionnaireNotFoundException;
 import com.epam.hackathon.emotional.help.model.Questionnaire;
 import com.epam.hackathon.emotional.help.service.QuestionnaireService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +18,14 @@ public class QuestionnaireController {
 
     private final QuestionnaireService questionnaireService;
 
+    private final ModelMapper mapper;
+
     @GetMapping
-    public Questionnaire findQuestionnaire(@RequestParam(name = "title") String title) {
-        return questionnaireService.findQuestionnaireByTitle(title)
+    public QuestionnaireDto findQuestionnaire(@RequestParam(name = "title") String title) {
+       Questionnaire questionnaire = questionnaireService.findQuestionnaireByTitle(title)
                 .orElseThrow(
                         () -> new QuestionnaireNotFoundException("Questionnaire with title - " + title + " not found"));
+        return mapper.map(questionnaire, QuestionnaireDto.class);
     }
 
 }
