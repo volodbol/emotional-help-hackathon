6b544491ebe76fe841836c9539f240efc77b1425
Kudakster <vitaliy.chervatyuk@gmail.com>
2022-12-01 12:34:27 +0200

    fix: return selected import

--- src/main/java/com/epam/hackathon/emotional/help/model/ApplicationUser.java
+++ src/main/java/com/epam/hackathon/emotional/help/model/ApplicationUser.java
@@ -1,8 +1,18 @@
 package com.epam.hackathon.emotional.help.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "application_user")
