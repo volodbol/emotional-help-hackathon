7fc7e01e92415cbf96a7399d3844fb9011f3ad09
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-09 13:45:13 +0200

    fix: change emotional map mapper to compute percent values

--- src/main/java/com/epam/hackathon/emotional/help/mapper/EmotionalMapMapper.java
+++ src/main/java/com/epam/hackathon/emotional/help/mapper/EmotionalMapMapper.java
@@ -4,21 +4,45 @@ import com.epam.hackathon.emotional.help.dto.DiagramValueDto;
 import com.epam.hackathon.emotional.help.model.EmotionalMap;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @Component
 public class EmotionalMapMapper {
 
+    private static final int ONE_HUNDRED_PERCENT = 100;
+
+    private static final int PERCENT_VALUE_SCALE = 2;
+
     public List<DiagramValueDto> toDiagramValueDtos(EmotionalMap emotionalMap) {
-        return List.of(
-                new DiagramValueDto("Fear", emotionalMap.getFearValue()),
-                new DiagramValueDto("Angry", emotionalMap.getAngryValue()),
-                new DiagramValueDto("Sadness", emotionalMap.getSadnessValue()),
-                new DiagramValueDto("Joy", emotionalMap.getJoyValue()),
-                new DiagramValueDto("Surprise", emotionalMap.getSurpriseValue()),
-                new DiagramValueDto("Happy", emotionalMap.getHappyValue())
+        List<DiagramValueDto> diagramValueDtos = List.of(
+                new DiagramValueDto("Fear", emotionalMap.getFearValue().doubleValue()),
+                new DiagramValueDto("Angry", emotionalMap.getAngryValue().doubleValue()),
+                new DiagramValueDto("Sadness", emotionalMap.getSadnessValue().doubleValue()),
+                new DiagramValueDto("Joy", emotionalMap.getJoyValue().doubleValue()),
+                new DiagramValueDto("Surprise", emotionalMap.getSurpriseValue().doubleValue()),
+                new DiagramValueDto("Happy", emotionalMap.getHappyValue().doubleValue())
         );
+        BigDecimal oneValuePercent = getOneValuePercent(diagramValueDtos);
+        diagramValueDtos.forEach(diagramValue ->
+                diagramValue.setValue(convertToPercent(diagramValue.getValue(), oneValuePercent)));
+        return diagramValueDtos;
     }
 
+    private static BigDecimal getOneValuePercent(List<DiagramValueDto> diagramValueDtos) {
+        return diagramValueDtos.stream()
+                .map(DiagramValueDto::getValue)
+                .reduce(Double::sum)
+                .map(total -> BigDecimal.valueOf(ONE_HUNDRED_PERCENT / total))
+                .orElse(BigDecimal.valueOf(1.0));
+    }
+
+    private static Double convertToPercent(Double aDouble, BigDecimal oneValuePercent) {
+        return oneValuePercent
+                .multiply(BigDecimal.valueOf(aDouble))
+                .setScale(PERCENT_VALUE_SCALE, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
 
 }
