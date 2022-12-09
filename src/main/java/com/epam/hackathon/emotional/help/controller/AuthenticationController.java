28d5cbc24034e827710f2dd9036b98a8d6359630
Volodymyr Bolshakov <volodbol@gmail.com>
2022-12-09 13:42:19 +0200

    feat: add new logic for sign-up endpoint

--- src/main/java/com/epam/hackathon/emotional/help/controller/AuthenticationController.java
+++ src/main/java/com/epam/hackathon/emotional/help/controller/AuthenticationController.java
@@ -2,9 +2,13 @@ package com.epam.hackathon.emotional.help.controller;
 
 import com.epam.hackathon.emotional.help.dto.ApplicationUserCreationDto;
 import com.epam.hackathon.emotional.help.exception.UsernameOrPasswordIncorrectException;
+import com.epam.hackathon.emotional.help.model.ApplicationUser;
+import com.epam.hackathon.emotional.help.model.EmotionalMap;
 import com.epam.hackathon.emotional.help.service.ApplicationUserService;
+import com.epam.hackathon.emotional.help.service.EmotionalMapService;
 import com.epam.hackathon.emotional.help.service.UserAuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
+@CrossOrigin
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final UserAuthenticationService userAuthenticationService;
 
     private final ApplicationUserService applicationUserService;
+
+    private final EmotionalMapService emotionalMapService;
 
     @PostMapping("sign-in")
     public String login(@RequestParam String username, @RequestParam String password) {
@@ -27,8 +34,13 @@ public class AuthenticationController {
     }
 
     @PostMapping("sign-up")
-    public String register(@RequestBody ApplicationUserCreationDto userDto) {
-        applicationUserService.saveApplicationUser(userDto);
+    public String register(@RequestBody ApplicationUserCreationDto userDto, @RequestParam String anonymousUuid) {
+        ApplicationUser applicationUser = applicationUserService.saveApplicationUser(userDto);
+        emotionalMapService.findEmotionalMapById(anonymousUuid)
+                .ifPresent((EmotionalMap emotionalMap) -> {
+                    emotionalMap.setApplicationUser(applicationUser);
+                    emotionalMapService.saveEmotionalMap(emotionalMap);
+                });
         return login(userDto.getUsername(), userDto.getPassword());
     }
 
